055a909e6794c34a5b7bc43f2f84f9c7c281e89e
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-07-19 13:12:28 +0300

    style: Conform to the styling guides (GNU 89 and Betty)

--- execute.c
+++ execute.c
@@ -8,21 +8,19 @@
 void execute(char *c)
 {
 		pid_t pid = fork();
-		extern char **environ;
 		char *t_cmd = path(c);
-		char *args[] = {t_cmd,NULL};
-		printf("%s\n", t_cmd);
+		char *args[] = {NULL};
 
 		if (pid < 0)
 		{
-				perror("fork");
-				_exit(EXIT_FAILURE);
+			perror("fork");
+			_exit(EXIT_FAILURE);
 		}
 		else if (pid == 0)
 		{
-				execve(args[0], args, environ);
-				perror(c);
-				_exit(EXIT_FAILURE);
+			execve(t_cmd, args, environ);
+			perror(c);
+			_exit(EXIT_FAILURE);
 		}
 		else
 		{
